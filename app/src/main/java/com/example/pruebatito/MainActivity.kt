d876703c9af7f4a02531c5d17fdcac6482863aab
Sarai G <sarairivera005@gmail.com>
2024-09-16 18:30:46 -0600

    Cambio de nombre a Titoo

--- app/src/main/java/com/example/pruebatito/MainActivity.kt
+++ app/src/main/java/com/example/pruebatito/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             PruebaTitoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Titoo",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     PruebaTitoTheme {
-        Greeting("Android")
+        Greeting("Titoo")
     }
 }
